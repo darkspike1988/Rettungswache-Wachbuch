740d193f3a494b43c9fc5a3860d1fced45b2c57c
darkspike1988 <impedire@googlemail.com>
2026-08-09 23:42:27 +0200

    fix(db): merge waste calendar and wachalltag migration branches

---
+++ core/migrations/0020_merge_0019_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0019_waste_calendar"),
+        ("core", "0019_wachalltag_models"),
+    ]
+
+    operations = []
